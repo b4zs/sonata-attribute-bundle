0822a6fe40b9284ff95a35de69da6cacbe495850
Balazs Varga <b4zs@b4zs.com>
2017-10-19 17:17:12 +0200

    clone fix

--- Entity/Attribute.php
+++ Entity/Attribute.php
@@ -4,6 +4,8 @@
 namespace Core\AttributeBundle\Entity;
 
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 abstract class Attribute
 {
 	private $id;
@@ -63,4 +65,21 @@ abstract class Attribute
 	{
 		return $this->getValue();
 	}
+
+    /**
+     * When an object is cloned, PHP 5 will perform a shallow copy of all of the object's properties.
+     * Any properties that are references to other variables, will remain references.
+     * Once the cloning is complete, if a __clone() method is defined,
+     * then the newly created object's __clone() method will be called, to allow any necessary properties that need to be changed.
+     * NOT CALLABLE DIRECTLY.
+     *
+     * @return mixed
+     * @link http://php.net/manual/en/language.oop5.cloning.php
+     */
+    public function __clone()
+    {
+        $this->id = null;
+    }
+
+
 }
--- Entity/CollectionAttribute.php
+++ Entity/CollectionAttribute.php
@@ -9,73 +9,73 @@ use Doctrine\Common\Collections\Collection;
 
 class CollectionAttribute extends Attribute
 {
-	/** @var  Collection|Attribute[] */
-	protected $collectionValue;
+    /** @var  Collection|Attribute[] */
+    protected $collectionValue;
 
-	function __construct()
-	{
-		$this->collectionValue = new ArrayCollection();
-	}
+    function __construct()
+    {
+        $this->collectionValue = new ArrayCollection();
+    }
 
-	function __toString()
-	{
-		return get_class($this->getValue());
-	}
+    function __toString()
+    {
+        return get_class($this->getValue());
+    }
 
-	public function getValue()
-	{
-		return $this->collectionValue;
-	}
+    public function getValue()
+    {
+        return $this->collectionValue;
+    }
 
-	public function setValue($value)
-	{
-		$this->assertValue($value);
-		$this->collectionValue = $value;
-	}
+    public function setValue($value)
+    {
+        $this->assertValue($value);
+        $this->collectionValue = $value;
+    }
 
-	public function __get($name)
-	{
-		foreach ($this->getValue() as $childAttribute) {
-			if ($childAttribute instanceof Attribute && $childAttribute->getType()) {
-				if ($childAttribute->getType()->getName() === $name) {
-					return $childAttribute;
-				}
-			}
-		}
+    public function __get($name)
+    {
+        foreach ($this->getValue() as $childAttribute) {
+            if ($childAttribute instanceof Attribute && $childAttribute->getType()) {
+                if ($childAttribute->getType()->getName() === $name) {
+                    return $childAttribute;
+                }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	function __set($name, $value)
-	{
-		if ($value instanceof Attribute) {
-			if ($name !== $value->getType()->getName()) {
-				throw new \RuntimeException('property name and attribute.type.name differs');
-			}
+    function __set($name, $value)
+    {
+        if ($value instanceof Attribute) {
+            if ($name !== $value->getType()->getName()) {
+                throw new \RuntimeException('property name and attribute.type.name differs');
+            }
 
-			foreach($this->getValue() as $key => $child){
-				if($child->getType()->getName() == $value->getType()->getName()){
-					$this->getValue()->remove($key);
-					break;
-				}
-			}
+            foreach ($this->getValue() as $key => $child) {
+                if ($child->getType()->getName() == $value->getType()->getName()) {
+                    $this->getValue()->remove($key);
+                    break;
+                }
+            }
 
-			$value->setParent($this);
-			$this->getValue()->add($value);
-		} else {
-			throw new \InvalidArgumentException('Argument 2. must be an instance of Attribute');
-		}
-	}
+            $value->setParent($this);
+            $this->getValue()->add($value);
+        } else {
+            throw new \InvalidArgumentException('Argument 2. must be an instance of Attribute');
+        }
+    }
 
-	public function assertValue($value)
-	{
-		if (!$value instanceof Collection ) {
-			throw $this->createValueAssertionException('an instance of Doctrine\\Common\\Collections\\Collection', $value);
-		}
-	}
+    public function assertValue($value)
+    {
+        if (!$value instanceof Collection) {
+            throw $this->createValueAssertionException('an instance of Doctrine\\Common\\Collections\\Collection', $value);
+        }
+    }
 
-	public function serialize()
-	{
+    public function serialize()
+    {
 
         if (!$this->getValue() instanceof Collection) {
             return null;
@@ -92,6 +92,19 @@ class CollectionAttribute extends Attribute
             }
             return $result;
         }
-	}
+    }
 
+    public function __clone()
+    {
+        parent::__clone();
+
+        $newCollection = new ArrayCollection();
+
+        $this->collectionValue->map(function(Attribute $attribute) use ($newCollection ) {
+            $newAttribute = clone $attribute;
+            $newAttribute->setParent($this);
+            $newCollection->add($newAttribute);
+        });
+        $this->collectionValue = $newCollection ;
+    }
 }
