6a939cb70bf358e03295eaeee957659f8421b01b
davidbecsei <becsei.david@gb-art.hu>
2018-01-31 13:57:23 +0100

    hotfix

--- DependencyInjection/CoreAttributeExtension.php
+++ DependencyInjection/CoreAttributeExtension.php
@@ -24,66 +24,69 @@ class CoreAttributeExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
 
         if ($container->hasParameter('sonata.media.admin.media.entity')) {
             $mediaClass = $container->getParameter('sonata.media.admin.media.entity');
             $galleryClass = $container->getParameter('sonata.media.admin.gallery.entity');
-
-            $this->registerDoctrineMapping(array(
-                'media_class'   => $mediaClass,
-                'gallery_class' => $galleryClass,
-            ));
-
         }
 
         if ($container->hasParameter('sonata.classification.admin.category.entity')) {
             $categoryClass = $container->getParameter('sonata.classification.admin.category.entity');
-            $this->registerDoctrineMapping(array(
-                'category_class' => $categoryClass
-            ));
         }
+
+        $this->registerDoctrineMapping(array(
+            'media_class' => $mediaClass,
+            'gallery_class' => $galleryClass,
+            'category_class' => $categoryClass
+        ));
     }
 
     private function registerDoctrineMapping($config)
     {
         $collector = DoctrineCollector::getInstance();
 
-        $collector->addAssociation('Core\AttributeBundle\Entity\MediaAttribute', 'mapManyToOne', array(
-            'fieldName'     => 'mediaValue',
-            'targetEntity'  => $config['media_class'],
-            'cascade'       => array('persist', 'refresh', 'merge'),
-            'joinColumns'    => array(
-                array(
-                    'name'     => 'media_value',
-                    'onDelete' => 'SET NULL',
+        if (!empty($config['media_class'])) {
+            $collector->addAssociation('Core\AttributeBundle\Entity\MediaAttribute', 'mapManyToOne', array(
+                'fieldName' => 'mediaValue',
+                'targetEntity' => $config['media_class'],
+                'cascade' => array('persist', 'refresh', 'merge'),
+                'joinColumns' => array(
+                    array(
+                        'name' => 'media_value',
+                        'onDelete' => 'SET NULL',
+                    ),
                 ),
-            ),
-        ));
-        
-        $collector->addAssociation(CategoryAttribute::class, 'mapManyToOne', array(
-            'fieldName'     => 'categoryValue',
-            'targetEntity'  => $config['category_class'],
-            'cascade'       => array('persist', 'refresh', 'merge'),
-            'joinColumns'   => array(
-                array(
-                    'name'      => 'media_value',
-                    'onDelete'  => 'setNull'
-                ),
-            ),
-        ));
+            ));
+        }
 
-        $collector->addAssociation('Core\AttributeBundle\Entity\GalleryAttribute', 'mapManyToOne', array(
-            'fieldName'     => 'galleryValue',
-            'targetEntity'  => $config['gallery_class'],
-            'cascade'       => array('persist', 'refresh', 'merge'),
-            'joinColumns'    => array(
-                array(
-                    'name'     => 'gallery_value',
-                    'onDelete' => 'SET NULL',
+        if (!empty($config['gallery_class'])) {
+            $collector->addAssociation(CategoryAttribute::class, 'mapManyToOne', array(
+                'fieldName' => 'categoryValue',
+                'targetEntity' => $config['category_class'],
+                'cascade' => array('persist', 'refresh', 'merge'),
+                'joinColumns' => array(
+                    array(
+                        'name' => 'media_value',
+                        'onDelete' => 'setNull'
+                    ),
                 ),
-            ),
-        ));
+            ));
+        }
+
+        if (!empty($config['category_class'])) {
+            $collector->addAssociation('Core\AttributeBundle\Entity\GalleryAttribute', 'mapManyToOne', array(
+                'fieldName' => 'galleryValue',
+                'targetEntity' => $config['gallery_class'],
+                'cascade' => array('persist', 'refresh', 'merge'),
+                'joinColumns' => array(
+                    array(
+                        'name' => 'gallery_value',
+                        'onDelete' => 'SET NULL',
+                    ),
+                ),
+            ));
+        }
     }
 }
